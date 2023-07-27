6f9818710ab1444c54614e72d15619a86c87bc15
atalea <abdulelah.s.1992@gmail.com>
2023-07-27 17:44:28 -0600

    changed model and some global params

--- fl_sim_MNIST.py
+++ fl_sim_MNIST.py
@@ -6,26 +6,28 @@ import random
 import heapq
 import numpy as np
 import matplotlib.pyplot as plt
+import torch.nn.functional as F
 
 # Define the model architecture
 
 
-class CNNModel(nn.Module):
+class CNNMnist(nn.Module):
     def __init__(self):
-        super(CNNModel, self).__init__()
-        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.fc1 = nn.Linear(32 * 7 * 7, 64)
-        self.fc2 = nn.Linear(64, 10)
+        super(CNNMnist, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
 
     def forward(self, x):
-        x = self.pool(nn.functional.relu(self.conv1(x)))
-        x = self.pool(nn.functional.relu(self.conv2(x)))
-        x = x.view(-1, 32 * 7 * 7)
-        x = nn.functional.relu(self.fc1(x))
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, x.shape[1]*x.shape[2]*x.shape[3])
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
         x = self.fc2(x)
-        return x
+        return F.log_softmax(x, dim=1)
 
 # Function to train the model on local data and calculate accuracy
 
@@ -80,7 +82,7 @@ def test(model, test_loader, criterion):
 def federated_learningFedAvg(local_epochs, global_epochs,  learning_rate):
     temp_power = []
     # Create the global model
-    global_model = CNNModel()
+    global_model = CNNMnist()
     criterion = nn.CrossEntropyLoss()
 
     # Load the MNIST dataset
@@ -134,7 +136,7 @@ def federated_learningFedAvg(local_epochs, global_epochs,  learning_rate):
 
             for i in range(len(successfull_users)):
                 # print(f"This is user {i+1}, their ID is: {active[i]}")
-                local_model = CNNModel()
+                local_model = CNNMnist()
                 local_model.load_state_dict(global_model.state_dict())
 
                 # Get the local client data
@@ -167,7 +169,7 @@ def federated_learningFedAvg(local_epochs, global_epochs,  learning_rate):
 def federated_learningIBCS(local_epochs, global_epochs,  learning_rate):
     # Create the global model
     temp_power = []
-    global_model = CNNModel()
+    global_model = CNNMnist()
     criterion = nn.CrossEntropyLoss()
 
     # Load the MNIST dataset
@@ -219,7 +221,7 @@ def federated_learningIBCS(local_epochs, global_epochs,  learning_rate):
 
             for i in range(len(successfull_users)):
                 # print(f"This is user {i+1}, their ID is: {active[i]}")
-                local_model = CNNModel()
+                local_model = CNNMnist()
                 local_model.load_state_dict(global_model.state_dict())
 
                 # Get the local client data
@@ -273,9 +275,9 @@ def power(clients):
 num_clients = int(input('Please Enter the number of clients: '))
 clients = clients_pool(num_clients)
 clients_power = power(clients)
-global_epochs = 10
-local_epochs = 20
-learning_rate = 0.1
+global_epochs = 20
+local_epochs = 30
+learning_rate = 0.01
 top_k = 50
 state_0 = [0.9449, 0.0087, 0.9913]
 state_1 = [0.0551, 0.8509, 0.1491]
